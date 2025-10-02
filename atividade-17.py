a123fb15df5525febb288990e9febbe417f6009e
Wellincleiton Silva <wellincleitons@gmail.com>
2025-10-02 13:22:37 -0300

    Add files via upload

---
+++ atividade-17.py
@@ -0,0 +1,4 @@
+# Crie um dicionário com as informações de um aluno: nome, idade, curso. Exiba os valores.
+
+dicionario = {'nome': 'Wellik', 'idade' : 16, 'Curso': 'Desenvolvimento de Sistemas'}
+print(dicionario)    
